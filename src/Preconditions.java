cad39370dbdabd1545eeb3313ee051273f70c6af
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-02-22 08:55:28 +0100

    premier

--- src/Preconditions.java
+++ src/Preconditions.java
@@ -1,2 +1,10 @@
-public class Preconditions {
+public final class Preconditions {
+
+    private Preconditions() {}
+
+    public void checkArgument(boolean shouldBeTrue) {
+        if (!shouldBeTrue) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
