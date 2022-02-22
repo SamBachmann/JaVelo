3b94b946e48fb722c32adf0223475be8f3a7f892
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2022-02-22 09:43:26 +0100

    fichiers xml

---
+++ src/ch/epfl/javelo/Math2.java
@@ -0,0 +1,13 @@
+package ch.epfl.javelo;
+
+public final class Math2 {
+    private Math2 (){
+
+    }
+
+    /*public static int ceilDiv(int x, int y){
+
+        return;
+    }
+    public static double interpolate(double y0, double y1, double x){}*/
+}
