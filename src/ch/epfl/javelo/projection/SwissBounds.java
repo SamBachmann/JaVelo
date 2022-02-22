a7d090d080a19b6cf4906f87a3e5eb13fa9caee9
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-02-22 14:37:11 +0100

    La classe SwissBounds (3.4) est faite.

--- src/ch/epfl/javelo/projection/SwissBounds.java
+++ src/ch/epfl/javelo/projection/SwissBounds.java
@@ -1,4 +1,17 @@
 package ch.epfl.javelo.projection;
 
 public class SwissBounds {
+
+    public static final double MIN_E = 2485000;
+    public static final double MAX_E = 2834000;
+    public static final double MIN_N = 1075000;
+    public static final double MAX_N = 1296000;
+    public static final double WIDTH = MAX_E - MIN_E;
+    public static final double HEIGHT = MAX_N - MIN_N;
+
+    public static boolean containsEN(double e, double n) {
+
+        return e <= MAX_E && e >= MIN_E && n <= MAX_N && n >= MIN_N;
+    }
+
 }
