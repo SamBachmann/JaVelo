1b016f321778f778c2827c5a5531c66e855d7b06
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-02-25 13:35:37 +0100

    Méthode SwissBounds terminée.

--- src/ch/epfl/javelo/projection/SwissBounds.java
+++ src/ch/epfl/javelo/projection/SwissBounds.java
@@ -10,8 +10,7 @@ package ch.epfl.javelo.projection;
 
 public final class SwissBounds {
 
-    // Valeurs constantes permettant de délimiter
-
+    // Valeurs constantes permettant de délimiter les frontières de la Suisse.
     public static final double MIN_E = 2485000;
     public static final double MAX_E = 2834000;
     public static final double MIN_N = 1075000;
@@ -19,6 +18,14 @@ public final class SwissBounds {
     public static final double WIDTH = MAX_E - MIN_E;
     public static final double HEIGHT = MAX_N - MIN_N;
 
+    /**
+     *
+     * Méthode nous permettant de savoir si des coordonnées sont bien en Suisse.
+     *
+     * @param e Cet argument désigne la coordonnée E dans le système de coordonnées suisse.
+     * @param n Cet argument désigne la coordonnée N dans le système de coordonnées suisse.
+     * @return Un booléen indiquant si lesdites coordonnées sont bien en Suisse.
+     */
     public static boolean containsEN(double e, double n) {
 
         return e <= MAX_E && e >= MIN_E && n <= MAX_N && n >= MIN_N;
