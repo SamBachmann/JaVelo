54a38186c8a0587109cdffb16013109b98b9baf3
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-02-25 13:26:03 +0100

    Méthode Preconditions terminée.

--- src/ch/epfl/javelo/Preconditions.java
+++ src/ch/epfl/javelo/Preconditions.java
@@ -1,8 +1,25 @@
 package ch.epfl.javelo;
 
+/**
+ *
+ * @author Samuel Bachmann (340373)
+ * @author Cyrus Giblain (312042)
+ *
+ * Le 25/02/2022
+ */
+
 public final class Preconditions {
 
+    // Un constructeur privé pour cette classe non instantiable.
     private Preconditions() {}
+
+    /**
+     *
+     * Méthode permettant de vérifier l'argument shouldBeTrue.
+     *
+     * @param shouldBeTrue L'argument qui doit être vrai pour qu'une
+     *                     exception ne soit pas levée.
+     */
 
     public static void checkArgument(boolean shouldBeTrue) {
         if (!shouldBeTrue) {
