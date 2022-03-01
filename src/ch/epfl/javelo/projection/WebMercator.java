440563cd9a8b4343e069168c1ff772889209b94c
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-03-01 09:17:10 +0100

    Méthode WebMercator terminée.

---
+++ src/ch/epfl/javelo/projection/WebMercator.java
@@ -0,0 +1,57 @@
+package ch.epfl.javelo.projection;
+
+
+import ch.epfl.javelo.Math2;
+
+/**
+ *
+ * @author Samuel Bachmann (340373)
+ * @author Cyrus Giblain (312042)
+ *
+ * Le 01/03/2022
+ */
+public final class WebMercator {
+
+    /**
+     *
+     * Méthode nous permettant trouver la coordonnée x dans le système Web Mercator.
+     *
+     * @param lon Cet argument désigne la longitude, donnée en radians dans le système WGS 84.
+     * @return La coordonnée x dans le système Web Mercator.
+     */
+    public double x(double lon) {
+       return (lon + Math.PI)/(2 * Math.PI);
+    }
+    /**
+     *
+     * Méthode nous permettant trouver la coordonnée y dans le système Web Mercator.
+     *
+     * @param lat Cet argument désigne la latitude, donnée en radians dans le système WGS 84.
+     * @return La coordonnée y dans le système Web Mercator.
+     */
+    public double y(double lat) {
+        return (Math.PI - Math2.asinh(Math.tan(lat)))/(2 * Math.PI);
+    }
+
+    /**
+     *
+     * Méthode nous permettant trouver la longitude dans le système WGS 84.
+     *
+     * @param x Cet argument désigne la coordonnée x, donnée dans le système Web Mercator.
+     * @return La longitude, en radians, dans le système WGS 84.
+     */
+    public double lon(double x) {
+        return (2 * Math.PI * x) - Math.PI;
+    }
+
+    /**
+     *
+     * Méthode nous permettant trouver la latitude dans le système WGS 84.
+     *
+     * @param y Cet argument désigne la coordonnée y, donnée dans le système Web Mercator.
+     * @return La latitude, en radians, dans le système WGS 84.
+     */
+    public double lat(double y) {
+        return Math.atan(Math.sinh(Math.PI - (2 * Math.PI * y)));
+    }
+}
