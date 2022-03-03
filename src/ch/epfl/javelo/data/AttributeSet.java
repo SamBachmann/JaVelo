a1527c039149881b2aa5e504c25ecf8796f1422c
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-03-03 01:14:25 +0100

    classe 3.7 terminée.

--- src/ch/epfl/javelo/data/AttributeSet.java
+++ src/ch/epfl/javelo/data/AttributeSet.java
@@ -2,6 +2,8 @@ package ch.epfl.javelo.data;
 
 import ch.epfl.javelo.Preconditions;
 
+import java.util.StringJoiner;
+
 public record AttributeSet(long bits) {
 
     public AttributeSet {
@@ -11,10 +13,40 @@ public record AttributeSet(long bits) {
 
     public static AttributeSet of(Attribute... attributes) {
         long newbits = 0;
-        for (int i = 0; i < attributes.length; ++i) {
-            long bitsTemporaire = 1;
-
+        for (Attribute attribute : attributes) {
+            long mask = 1L << attribute.ordinal();
+            newbits = newbits | mask;
         }
         return new AttributeSet(newbits);
+    }
+
+    public boolean contains(Attribute attribute) {
+        long mask = 1L << attribute.ordinal();
+        return  (bits & mask) == mask;
+    }
+
+    public boolean intersects(AttributeSet that) {
+
+        long mask = 1L;
+        boolean intersection = false;
+        for (int i = 0; i < 64; ++i) {
+            intersection = (mask & this.bits & that.bits) == mask;
+            if (intersection) {
+                break;
+            }
+            mask = mask << 1;
+        }
+        return intersection;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner j = new StringJoiner(",","{","}");
+        for (Attribute attribut : Attribute.ALL) {
+            if (this.contains(attribut)) {
+                j.add(attribut.keyValue());
+            }
+        }
+        return j.toString();
     }
 }
