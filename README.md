443f41f1372cde35cc8aaf5aaac398351c49ecc0
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2022-03-09 14:27:22 +0100

    Corrections dans Functions Création de GraphEdges

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ Repository for the PPOO JaVelo project
 
 
 Cyrus: <br>
-- [ ] 3.2
+- 3.4.1
+- 3.4.2
+- 3.4.3
+- 3.4.7
 
 Samuel:<br>
-- [ ] 3.3
+- 3.4.4
+- 3.4.5
+- 3.4.6
---
+++ src/ch/epfl/javelo/data/GraphEdges.java
@@ -0,0 +1,13 @@
+package ch.epfl.javelo.data;
+
+/**
+ *
+ *
+ *  @author Samuel Bachmann (340373)
+ *  @author Cyrus Giblain (312042)
+ * <br>
+ * 09/03/2022
+ */
+public record GraphEdges() {
+
+}
