043c165b267a156a8a417c8fa5a4d698d458f6dd
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2022-03-15 11:33:14 +0100

    Création de la classe ElevationProfile

---
+++ src/ch/epfl/javelo/routing/ElevationProfile.java
@@ -0,0 +1,17 @@
+package ch.epfl.javelo.routing;
+
+/**
+ * Classe immuable représentant le profil en long d'un itinéraire, simple ou multiple.
+ *
+ *  @author Samuel Bachmann (340373)
+ *  @author Cyrus Giblain (312042)
+ * <br>
+ * 15/03/2022
+ */
+
+public final class ElevationProfile {
+    public ElevationProfile(double length, float[] elevationSamples){
+
+    }
+
+}
