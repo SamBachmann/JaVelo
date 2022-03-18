c415ec9b507f410288c4a7c9207e3486dee03ef5
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-03-18 13:58:33 +0100

    13:58

--- src/ch/epfl/javelo/data/Graph.java
+++ src/ch/epfl/javelo/data/Graph.java
@@ -2,7 +2,6 @@ package ch.epfl.javelo.data;
 
 import ch.epfl.javelo.Functions;
 import ch.epfl.javelo.projection.PointCh;
-
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
@@ -14,6 +13,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.DoubleUnaryOperator;
 
+/**
+ *
+ * Classe représentant un graphe de JaVelo, qui comporte des nœuds, des arêtes
+ * des secteurs et des ensembles d'attributs.
+ *
+ *  @author Samuel Bachmann (340373)
+ *  @author Cyrus Giblain (312042)
+ * <br>
+ * 18/03/2022
+ */
 public final class Graph {
 
     private final GraphNodes nodes;
@@ -30,6 +39,13 @@ public final class Graph {
         this.attributeSets = List.copyOf(attributeSets);
     }
 
+    /**
+     * Méthode permettant d'obtenir le graphe JaVelo obtenu à partir de fichiers
+     * dont on fournit le chemin d'accès.
+     *
+     * @param basePath Le chemin d'accès du fichier.
+     * @return Un graphe JaVelo obtenu à partir du fichier donné.
+     */
     public static Graph loadFrom(Path basePath) throws IOException {
 
         GraphNodes newNodes;
@@ -95,24 +111,59 @@ public final class Graph {
         return new Graph(newNodes, newSectors, newEdges, newAttributeSets);
     }
 
+    /**
+     * Méthode qui nous donne le nombre total de nœuds dans le graphe JaVelo.
+     *
+     * @return Un int représentant le nombre de nœuds du graphe JaVelo.
+     */
     public int nodeCount() {
         return this.nodes.count();
     }
 
+    /**
+     * Méthode qui nous donne la position du nœud d'identité donné.
+     *
+     * @param nodeId L'identité du nœud donnée.
+     * @return Un PointCh caractérisant la position du nœud d'identité donnée.
+     */
     public PointCh nodePoint(int nodeId) {
         double E = this.nodes.nodeE(nodeId);
         double N = this.nodes.nodeN(nodeId);
         return new PointCh(E, N);
     }
 
+    /**
+     * Méthode qui nous donne le nombre d'arêtes sortant du nœud d'identité donnée.
+     *
+     * @param nodeId L'identité du nœud donnée.
+     * @return le nombre d'arêtes sortant du nœud d'identité donnée.
+     */
     public int nodeOutDegree(int nodeId) {
         return this.nodes.outDegree(nodeId);
     }
 
+    /**
+     * Méthode qui nous donne l'identité de la edgeIndex-ème arête sortant
+     * du nœud d'identité donnée.
+     *
+     * @param nodeId L'identité du nœud en question.
+     * @param edgeIndex L'index de l'arête en question.
+     * @return Un int représentant l'identité de la edgeIndex-ème arête sortant du noeud d'identité
+     * nodeId.
+     */
     public int nodeOutEdgeId(int nodeId, int edgeIndex) {
         return this.nodes.edgeId(nodeId, edgeIndex);
     }
 
+    /**
+     * Méthode qui nous donne l'identité du nœud se trouvant le plus proche du point donné,
+     * à la distance maximale donnée.
+     *
+     * @param point Le point donné.
+     * @param searchDistance La distance maximale donnée.
+     * @return Un int représentant l'identité du nœud se trouvant le plus proche du point donné,
+     * à la distance maximale donnée, ou -1 si aucun nœud ne correspond à ces critères.
+     */
     public int nodeClosestTo(PointCh point, double searchDistance) {
 
         int nodeId = -1;
@@ -133,23 +184,58 @@ public final class Graph {
         return nodeId;
     }
 
+    /**
+     * Méthode nous donnant l'identité du noeud destination de l'arête d'identité
+     * donnée.
+     *
+     * @param edgeId L'identité de l'arête.
+     * @return Un int représentant l'identité du noeud destination de l'arête d'identité
+     * donnée.
+     */
     public int edgeTargetNodeId(int edgeId) {
         return this.edges.targetNodeId(edgeId);
     }
 
+    /**
+     * Méthode nous permettant de savoir si l'arête d'identité donnée va dans le sens
+     * contraire de la voie OSM dont elle provient.
+     *
+     * @param edgeId L'identité de l'arête.
+     * @return Un booléen nous permettant de savoir si l'arête d'identité donnée va
+     * dans le sens contraire de la voie OSM dont elle provient.
+     */
     public boolean edgeIsInverted(int edgeId) {
         return this.edges.isInverted(edgeId);
     }
 
+    /**
+     * Méthode nous donnant l'ensemble des attributs OSM attachés à l'arête d'identité
+     * donnée.
+     *
+     * @param edgeId L'identité de l'arête.
+     * @return L'ensemble d'attributs attachés à l'arête d'identité donnée.
+     */
     public AttributeSet edgeAttributes(int edgeId) {
         return new AttributeSet(this.edges
                 .attributesIndex(edgeId));
     }
 
+    /**
+     * Méthode nous donnant la longueur, en mètres, de l'arête d'identité donnée.
+     *
+     * @param edgeId L'identité de l'arête.
+     * @return Un double représentant la longueur, en mètres, de l'arête d'identité donnée.
+     */
     public double edgeLength(int edgeId) {
         return this.edges.length(edgeId);
     }
 
+    /**
+     * Méthode nous donnant le dénivelé positif total de l'arête d'identité donnée.
+     *
+     * @param edgeId L'identité de l'arête.
+     * @return Un double représentant le dénivelé positif total de l'arête d'identité donnée.
+     */
     public double edgeElevationGain(int edgeId) {
         return this.edges.elevationGain(edgeId);
     }
