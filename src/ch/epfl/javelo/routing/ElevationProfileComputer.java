2fb57d0b0e0b9eaf9176a18648bfeffc55e2e4d4
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2022-03-23 15:05:06 +0100

    Suite de ElevationProfileComputer

--- src/ch/epfl/javelo/routing/ElevationProfileComputer.java
+++ src/ch/epfl/javelo/routing/ElevationProfileComputer.java
@@ -4,6 +4,7 @@ package ch.epfl.javelo.routing;
 import ch.epfl.javelo.Preconditions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public final class ElevationProfileComputer {
     private ElevationProfileComputer(){}
@@ -12,9 +13,40 @@ public final class ElevationProfileComputer {
         Preconditions.checkArgument(maxStepLength > 0);
 
         int nbSamples = (int) Math.ceil(route.length() / maxStepLength) + 1;
-        ArrayList<Double> elevationTable = new ArrayList<>();
+        double espacement = route.length() / (nbSamples - 1);
+        float[] elevationTable = new float[nbSamples];
 
-        for (int position = 0; position < route.length(); ){}
+        //initialiser le tableau
+        for (int i = 0; i < nbSamples; ++i ){
+            double position = espacement * i;
+            elevationTable[i] = (float) route.elevationAt(position);
+        }
+
+        //Remplacer les valeurs NaN
+        //Remplir en tête de tableau
+        int k= 0;
+        while (k < elevationTable.length && Float.isNaN(elevationTable[k])){
+            k ++;
+        }
+
+        if (k == elevationTable.length){
+            Arrays.fill(elevationTable,0,k, 0f);
+        }else if (k > 0 && k < elevationTable.length){
+            Arrays.fill(elevationTable,0,k,elevationTable[k]);
+            //Remplir en queue de tableau.
+            int c = elevationTable.length;
+            do {
+                --c;
+            } while (c > 0 && Float.isNaN(elevationTable[c]));
+
+            if (c < elevationTable.length - 1){
+                Arrays.fill(elevationTable,c + 1,elevationTable.length, elevationTable[c]);
+            }
+        }
+
+        //Interpolation des trous intermédiaires.
+
+
 
         return null;
     }
