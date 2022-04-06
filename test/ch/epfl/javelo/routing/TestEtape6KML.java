03e5f62308dc4992a2c6404b30c6a333a756ba9d
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2022-04-06 14:28:27 +0200

    test RouteComputer

--- test/ch/epfl/javelo/routing/TestEtape6KML.java
+++ test/ch/epfl/javelo/routing/TestEtape6KML.java
@@ -12,11 +12,14 @@ public final class TestEtape6KML {
             Graph g = Graph.loadFrom(Path.of("ch_west"));
             CostFunction cf = new CityBikeCF(g);
             RouteComputer rc = new RouteComputer(g, cf);
-            long t0 = System.nanoTime();
-            Route r = rc.bestRouteBetween(2046055, 2694240);
-            System.out.printf("Itinéraire calculé en %d ms\n",
-                    (System.nanoTime() - t0) / 1_000_000);
-            System.out.println("longueur : " + r.length());
+            Route r = null;
+            for (int i = 0; i < 20; ++i) {
+                long t0 = System.nanoTime();
+                r = rc.bestRouteBetween(2046055, 2694240);
+                System.out.printf(i + " : Itinéraire calculé en %d ms\n",
+                        (System.nanoTime() - t0) / 1_000_000);
+            }
+            //System.out.println("longueur : " + r.length());
             KmlPrinter.write("C:\\Users\\samue\\Documents\\EPFL\\Cours\\BA2\\PPOO\\JaVelo\\test\\ch\\epfl\\javelo\\routing\\javelo.kml", r);
         }
 
