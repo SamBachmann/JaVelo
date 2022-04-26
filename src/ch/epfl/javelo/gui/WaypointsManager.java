dfbacc3885ba4686a3ac693cddc9c141f3d03241
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2022-04-26 21:13:47 +0200

    Création de WaypointsManager

---
+++ src/ch/epfl/javelo/gui/WaypointsManager.java
@@ -0,0 +1,17 @@
+package ch.epfl.javelo.gui;
+
+import ch.epfl.javelo.data.Graph;
+
+/**
+ * Classe gérant l'affichage et les interactions des points de passages d'un itinéraire.
+ *
+ *  @author Samuel Bachmann (340373)
+ *  @author Cyrus Giblain (312042)
+ * <br>
+ * 26/04/2022
+ */
+public final class WaypointsManager {
+
+    public WaypointsManager (Graph graph ){}
+
+}
