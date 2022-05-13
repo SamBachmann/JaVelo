d995e9475e151ac164159d541b64c14e40899533
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-05-13 12:33:06 +0200

    12:33

--- src/ch/epfl/javelo/gui/RouteBean.java
+++ src/ch/epfl/javelo/gui/RouteBean.java
@@ -1,12 +1,10 @@
 package ch.epfl.javelo.gui;
 
-import ch.epfl.javelo.routing.ElevationProfile;
-import ch.epfl.javelo.routing.ElevationProfileComputer;
-import ch.epfl.javelo.routing.Route;
-import ch.epfl.javelo.routing.RouteComputer;
+import ch.epfl.javelo.routing.*;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
@@ -19,6 +17,8 @@ import javafx.collections.ObservableList;
  * <br>
  * 11/05/2022
  */
+import java.util.*;
+
 public final class RouteBean {
 
     public static final int DISTANCE_MAX_ECHANTILLONS = 5;
@@ -27,27 +27,54 @@ public final class RouteBean {
     private ObjectProperty<Route> route;
     private DoubleProperty highlightedPosition;
     private ObjectProperty<ElevationProfile> elevationProfil;
+    private final Map< Map<Integer, Integer>, Route> cacheItineraires =
+            new LinkedHashMap<>(100, 0.75f, true);
 
-
-
-    public RouteBean(RouteComputer routeComputer) {
+    public RouteBean(RouteComputer routeComputer, ObservableList<Waypoint> waypointsList) {
         this.routeComputer = routeComputer;
+        this.waypointsList = waypointsList;
+        List<Route> listeDeRoutes = new ArrayList<>();
 
         waypointsList.addListener((ListChangeListener<? super Waypoint>) observable -> {
             if (waypointsList.size() >= 2) {
                 for (int i = 0; i < waypointsList.size() - 1; ++i) {
-                    //int startNodeIndex = waypointsList.get(i).nodeId;
+                    int startNodeIndex = waypointsList.get(i).nodeId();
+                    int endNodeIndex = waypointsList.get(i + 1).nodeId();
+                    Map<Integer, Integer> nodesId = new HashMap<>();
+                    nodesId.put(startNodeIndex, endNodeIndex);
 
+                    Route routeX;
+
+                    if (cacheItineraires.containsKey(nodesId)) {
+                        routeX = cacheItineraires.get(nodesId);
+                    } else {
+                        if (cacheItineraires.size() >= 100) {
+                            Iterator<Map<Integer, Integer>> iterator = cacheItineraires.keySet().iterator();
+                            cacheItineraires.remove(iterator.next());
+                        }
+                        routeX = routeComputer.bestRouteBetween(startNodeIndex, endNodeIndex);
+                    }
+                    if (routeX != null) {
+                        listeDeRoutes.add(routeX);
+                        cacheItineraires.put(nodesId, routeX);
+                    } else {
+                        listeDeRoutes.clear();
+                        this.route = null;
+                        this.highlightedPosition = null;
+                        this.elevationProfil = null;
+                        break;
+                    }
+                }
+                Route multiRoute = new MultiRoute(listeDeRoutes);
+                this.route = new SimpleObjectProperty<>(multiRoute);
                 }
 
                 this.elevationProfil.set(
                         ElevationProfileComputer.elevationProfile(route.get(), DISTANCE_MAX_ECHANTILLONS)
                 );
-
-            }
-
         });
     }
+
 
 
     public DoubleProperty highlightedPositionProperty(){
@@ -62,7 +89,7 @@ public final class RouteBean {
         this.highlightedPosition.set(highlightedPosition);
     }
 
-    public ReadOnlyObjectProperty routeProperty(){
+    public ReadOnlyObjectProperty<Route> routeProperty(){
         return route;
     }
 
@@ -70,7 +97,7 @@ public final class RouteBean {
         return route.get();
     }
 
-    public ReadOnlyObjectProperty elevationProfilProperty(){
+    public ReadOnlyObjectProperty<ElevationProfile> elevationProfilProperty(){
         return elevationProfil;
     }
 }
