63082e55cdbf706590025163580207970e9a81d0
SamBachmann <93264382+SamBachmann@users.noreply.github.com>
2022-05-17 10:04:16 +0200

    avancée mardi 17.05

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -5,7 +5,16 @@ import ch.epfl.javelo.routing.ElevationProfile;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
+
 
 /**
  * Classe gérant l'affichage et les interactions avec le profil en long d'un itinéraire.
@@ -17,6 +26,7 @@ import javafx.scene.layout.Pane;
  */
 public final class ElevationProfileManager {
 
+    private final BorderPane borderPane;
     private final Pane pane;
 
     /**
@@ -26,13 +36,53 @@ public final class ElevationProfileManager {
      * @param positionProfil Position en évidence.
      */
     public ElevationProfileManager(ReadOnlyObjectProperty<ElevationProfile> profil,
-                                   ReadOnlyDoubleProperty positionProfil){
+                                ReadOnlyDoubleProperty positionProfil){
         this.pane = new Pane();
-        this.pane.getStylesheets().add("elevation_profile.css");
+        this.borderPane = new BorderPane();
+
+        this.borderPane.getStylesheets().add("elevation_profile.css");
+        this.borderPane.setCenter(this.pane);
+
+        VBox vBox = new VBox();
+        vBox.setId("profile_data");
+        Insets insets = new Insets(10, 10, 20, 40);
+        double hauteur = insets.getTop() - insets.getBottom();
+        double largeur = insets.getRight();
+
+        Text textVBox = new Text();
+        vBox.getChildren().add(textVBox);
+        
+        Path grille = new Path();
+        this.pane.getChildren().add(grille);
+        grille.setId("grid");
+
+        Group group = new Group();
+        this.pane.getChildren().add(group);
+
+        Text text1 = new Text();
+        text1.getStyleClass().add("grid_label");
+        text1.getStyleClass().add("horizontal");
+        group.getChildren().add(text1);
+
+        Text text2 = new Text();
+        text2.getStyleClass().add("grid_label");
+        text2.getStyleClass().add("vertical");
+        group.getChildren().add(text2);
+
+        Polygon profile = new Polygon();
+        profile.setId("profile");
+        this.pane.getChildren().add(profile);
+
+        Line line = new Line();
+        this.pane.getChildren().add(line);
+
+
+
+
     }
 
     public Pane pane(){
-        return this.pane;
+        return this.borderPane;
     }
 
     public ReadOnlyIntegerProperty mousePositionOnProfileProperty(){
