263bc6cfcdffc0b3250252c2c156d037507e31f2
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-05-19 13:48:11 +0200

    Ébauche de grille

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -2,6 +2,7 @@ package ch.epfl.javelo.gui;
 
 
 import ch.epfl.javelo.routing.ElevationProfile;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.*;
 import javafx.geometry.Insets;
 import javafx.geometry.Point2D;
@@ -10,9 +11,7 @@ import javafx.scene.Group;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.Polygon;
+import javafx.scene.shape.*;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Affine;
 import javafx.scene.transform.NonInvertibleTransformException;
@@ -42,36 +41,41 @@ public final class ElevationProfileManager {
     /**
      * Constructeur d'ElevationProfileManager
      *
-     * @param profil Profil de l'itinéraire.
+     * @param profil         Profil de l'itinéraire.
      * @param positionProfil Position en évidence.
      */
     public ElevationProfileManager(ReadOnlyObjectProperty<ElevationProfile> profil,
-                                ReadOnlyDoubleProperty positionProfil){
+                                   ReadOnlyDoubleProperty positionProfil) {
         this.pane = new Pane();
         this.borderPane = new BorderPane();
 
-        //Définir des dimentions par défauts si elles ne sont pas définies au démarage
+        //Définir des dimentions par défauts si elles ne sont pas définies au démarrage
         pane.setPrefWidth(600);
         pane.setPrefHeight(300);
 
         this.borderPane.getStylesheets().add("elevation_profile.css");
         this.borderPane.setCenter(this.pane);
 
-        dessinProfil = new Polygon();
+        this.dessinProfil = new Polygon();
         initHierarchie();
 
         Insets insets = new Insets(10, 10, 20, 40);
 
         dessineProfil(profil, insets);
 
-         Rectangle2D rectangle2D = new Rectangle2D(insets.getLeft(), insets.getTop(),
-                 Math.max(pane.getWidth() - insets.getRight() - insets.getLeft(), 0),
-                 Math.max(pane.getHeight() - insets.getTop() - insets.getBottom(), 0));
-         this.rectangleBleu.set(rectangle2D);
+        Rectangle2D rectangle2D = new Rectangle2D(insets.getLeft(), insets.getTop(),
+                Math.max(pane.getWidth() - insets.getRight() - insets.getLeft(), 0),
+                Math.max(pane.getHeight() - insets.getTop() - insets.getBottom(), 0));
+        this.rectangleBleu.set(rectangle2D);
 
+        pane.widthProperty().addListener(observable -> dessineProfil(profil, insets));
 
-         //Bindings.createObjectBinding() pour lier la ligne à la zone rectangle bleu
+        //Bindings.createObjectBinding() pour lier la ligne à la zone rectangle bleu
+        //this.borderPane.layoutXProperty().bind(Bindings.createDoubleBinding());
+        //this.borderPane.minp
+        this.borderPane.layoutXProperty().bind(borderPane.layoutXProperty());
     }
+
 
     /**
      * Méthode privée qui dessine le profil à partir d'un ElevationProfile et des
@@ -111,12 +115,41 @@ public final class ElevationProfileManager {
         for (int x = (int) p1.getX(); x <= p2.getX(); ++x ){
             double xItineraire = screenToWorld.transform(x, 0).getX();
             double elevationAtx = profil.get().elevationAt(xItineraire);
+
             Point2D pointAffiche = worldToScreen.get().transform(xItineraire, elevationAtx);
             System.out.printf("Altitude : %f Y écran : %f  \n", elevationAtx, pointAffiche.getY());
             listeDePoints.add(pointAffiche.getX());
             listeDePoints.add(pointAffiche.getY());
         }
+
         dessinProfil.getPoints().setAll(listeDePoints);
+
+
+        Path path = new Path();
+
+        double deltaElevation = worldToScreen.get().deltaTransform(0, profil.get().maxElevation() - profil.get().minElevation()).getY();
+
+        int[] POS_STEPS =
+                { 1000, 2000, 5000, 10_000, 25_000, 50_000, 100_000 };
+        int[] ELE_STEPS =
+                { 5, 10, 20, 25, 50, 100, 200, 250, 500, 1_000 };
+
+        int ecartAltitude = ELE_STEPS[9];
+
+        for (Integer step : ELE_STEPS) {
+            if ((deltaElevation / step) >= 25) {
+               ecartAltitude = step;
+               break;
+            }
+        }
+
+        double y = profil.get().minElevation();
+        while (y < profil.get().maxElevation()) {
+            y = y + ecartAltitude;
+            PathElement ligneextremite1 = new MoveTo(0,y);
+            PathElement ligneextremite2 = new LineTo(p2.getX(), y);
+        }
+
     }
 
 
@@ -153,6 +186,7 @@ public final class ElevationProfileManager {
 
         Line line = new Line();
         this.pane.getChildren().add(line);
+
     }
 
     /**
