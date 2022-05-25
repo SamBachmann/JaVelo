89735ad31972b56107af11473f33aefaa02e18fa
CyrusGiblain <58070502+CyrusGiblain@users.noreply.github.com>
2022-05-25 14:12:28 +0200

    Classe AnnotatedMapManager terminée.

--- src/ch/epfl/javelo/gui/JaVelo.java
+++ src/ch/epfl/javelo/gui/JaVelo.java
@@ -1,4 +1,26 @@
 package ch.epfl.javelo.gui;
 
-public final class JaVelo extends {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public final class JaVelo extends Application {
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages.
+     * @throws Exception if something goes wrong
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
